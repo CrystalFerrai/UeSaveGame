287e312cf817f7e6244aa400a236a880fa797cd7
Crystal <Crystal>
2023-02-10 17:02:43 -0500

    UDateTime now uses System.DateTime as a backing field

--- UeSaveGame/DataTypes/UDateTime.cs
+++ UeSaveGame/DataTypes/UDateTime.cs
@@ -1,14 +1,55 @@
-﻿namespace UeSaveGame.DataTypes
-{
-    public struct UDateTime
-    {
-        // See FDateTime in DateTime.h (UE4) for helpers to interpret ticks.
+﻿using System;
 
-        public long Ticks { get; set; }
+namespace UeSaveGame.DataTypes
+{
+    public struct UDateTime : IFormattable
+    {
+		private DateTime mDateTime;
+
+        public static UDateTime Now => new UDateTime(DateTime.Now);
+
+        public static UDateTime UtcNow => new UDateTime(DateTime.UtcNow);
+
+        public DateTime Value
+        {
+            get => mDateTime;
+            set => mDateTime = value;
+        }
+
+        public long Ticks
+		{
+			get => mDateTime.Ticks;
+			set => mDateTime = new DateTime(value);
+		}
+
+        public UDateTime(long ticks)
+		{
+            mDateTime = new DateTime(ticks);
+		}
+
+        public UDateTime(DateTime value)
+		{
+            mDateTime = value;
+		}
 
         public override string ToString()
         {
-            return Ticks.ToString();
+            return mDateTime.ToString();
+        }
+
+        public string ToString(string format)
+		{
+            return mDateTime.ToString(format);
+        }
+
+        public string ToString(IFormatProvider provider)
+        {
+            return mDateTime.ToString(provider);
+        }
+
+        public string ToString(string format, IFormatProvider provider)
+        {
+            return mDateTime.ToString(format, provider);
         }
     }
 }
